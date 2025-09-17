96b026cb8fbdeb766221092bdef12f75a8835c84
Fantomasa <ivailoinfo@gmail.com>
2025-09-17 15:06:18 +0300

    fix: the graph node deploy

--- subgraphs/zotto/zotto.sh
+++ subgraphs/zotto/zotto.sh
@@ -3,6 +3,6 @@ graph codegen subgraph.yaml
 
 graph build subgraph.yaml --network neura
 
-graph create --node https://deploy-testnet-graph-eth.infra.neuraprotocol.io/ pm
+graph create --node https://deploy-testnet-graph-neura.infra.neuraprotocol.io/ pm
 
 graph deploy --network neura --node https://deploy-testnet-graph-neura.infra.neuraprotocol.io/ --ipfs https://ipfs-testnet-graph-neura.infra.neuraprotocol.io pm subgraph.yaml
